8885ee989d210bb87dbd49cdfc510230103ac5b1
mateu <mateusz.migdal23@gmail.com>
2018-12-30 17:51:46 +0100

    Podpięta baza zadań + działajace tworzenie zadania

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -2,10 +2,19 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use AppBundle\Entity\Todo;
+#use Doctrine\DBAL\Types\DateTimeType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class TodoController extends Controller
 {
     /**
@@ -13,18 +22,66 @@ class TodoController extends Controller
      */
     public function listAction(Request $request)
     {
+        $todo = $this->getDoctrine()
+            ->getRepository('AppBundle:Todo')
+            ->findAll();
 
-        // replace this example code with whatever you need
-        return $this->render('todo/index.html.twig');
+        return $this->render('todo/index.html.twig', array(
+            'todo' =>$todo
+        ));
     }
     /**
      * @Route("/todo/create", name="todo_create")
      */
     public function createAction(Request $request)
     {
+        $todo = new Todo;
 
-        // replace this example code with whatever you need
-        return $this->render('todo/create.html.twig');
+        $form = $this->createFormBuilder($todo)
+            ->add('name', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom-15px')))
+            ->add('category', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom-15px')))
+            ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom-15px')))
+            ->add('priority', ChoiceType::class, array('choices' => array('Low' => 'Low', 'Normal' => 'Normal', 'High' => 'High'), 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom-15px')))
+            ->add('due_date', DateTimeType::class, array('attr' => array('class' => 'formcontrol', 'style' => 'margin-bottom-15px')))
+            ->add('Add', SubmitType::class, array('label' => 'Create Todo', 'attr' => array('class' => 'btn btn-primary', 'style' => 'margin-bottom-15px')))
+
+            ->getForm();
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            // Get Data
+            $name = $form['name']->getData();
+            $category = $form['category']->getData();
+            $description = $form['description']->getData();
+            $priority = $form['priority']->getData();
+            $due_date = $form['due_date']->getData();
+
+            $now =new\DateTime('now');
+
+            $todo->setName($name);
+            $todo->setCategory($category);
+            $todo->setDescription($description);
+            $todo->setPriority($priority);
+            $todo->setDueDate($due_date);
+            $todo->setCreateDate($now);
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($todo);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Task Added'
+            );
+
+            return $this->redirectToRoute('todo_list');
+
+
+        }
+        return $this->render('todo/create.html.twig', array(
+            'form' =>$form->createView()
+        ));
     }
     /**
      * @Route("/todo/edit/{id}", name="todo_edit")
