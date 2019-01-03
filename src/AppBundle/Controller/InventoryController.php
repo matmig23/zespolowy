948e42949de1c113800b35cb41d497507604b928
mateu <mateusz.migdal23@gmail.com>
2019-01-03 12:46:22 +0100

    Przekierowanie przycisku rejestracji

--- src/AppBundle/Controller/InventoryController.php
+++ src/AppBundle/Controller/InventoryController.php
@@ -11,7 +11,37 @@ class InventoryController extends Controller
     /**
      * @Route("/Inventory/index", name="inventory_list")
      */
-    public function inventoryAction(Request $request)
+    public function listAction(Request $request)
+    {
+
+        // replace this example code with whatever you need
+        return $this->render('Inventory/index.html.twig');
+    }
+
+    /**
+     * @Route("/Inventory/edit", name="inventory_edit")
+     */
+    public function editAction(Request $request)
+    {
+
+        // replace this example code with whatever you need
+        return $this->render('Inventory/edit.html.twig');
+    }
+
+    /**
+     * @Route("/Inventory/details", name="inventory_details")
+     */
+    public function detailsAction(Request $request)
+    {
+
+        // replace this example code with whatever you need
+        return $this->render('Inventory/details.html.twig');
+    }
+
+    /**
+     * @Route("/Inventory/index", name="inventory_delete")
+     */
+    public function deleteAction(Request $request)
     {
 
         // replace this example code with whatever you need
---
+++ src/AppBundle/Entity/Inventory.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace AppBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Todo
+ *
+ * @ORM\Table(name="inventory")
+ * @ORM\Entity
+ */
+class Inventory
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="category", type="string", length=255)
+     */
+    private $category;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="priority", type="string", length=255)
+     */
+    private $priority;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="due_date", type="datetime")
+     */
+    private $dueDate;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="create_date", type="datetime")
+     */
+    private $createDate;
+
+
+}
+
